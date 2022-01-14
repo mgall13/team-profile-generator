049599d4b47b251b053f83ba79c492d3a1aba246
Kyro <mariogallardo1331@yahoo.com>
2022-01-14 11:34:06 -0600

    adding inquirer prompts for engineer

--- index.js
+++ index.js
@@ -1,51 +1,89 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const path = require('path');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
 const allEmployees = [];
 
 function addManager() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'managerName',
-                message: "What is the manager's name?"
-            },
-            {
-                type: 'input',
-                name: 'managerId',
-                message: "Please input your manager's ID."
-            },
-            {
-                type: 'input',
-                name: 'managerEmail',
-                message: "What is your manager's email?"
-            },
-            {
-                type: 'input',
-                name: 'officeNumber',
-                message: "What is your manager's office number?"
-            }
-        ])
-        .then((data) => {
-            const manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.officeNumber)
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the manager's name?",
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "Please input your manager's ID.",
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is your manager's email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your manager's office number?",
+      },
+    ])
+    .then((data) => {
+      const manager = new Manager(
+        data.managerName,
+        data.managerId,
+        data.managerEmail,
+        data.officeNumber
+      );
 
-            allEmployees.push(manager);
+      allEmployees.push(manager);
 
-            directory();
-        })
+      directory();
+    });
 }
 
-function directory() {
+function directory() {}
 
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is your engineer's name?",
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is your engineer's name?",
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is your engineer's email?",
+      },
+      {
+        type: "input",
+        name: "gitHub",
+        message: "What is your engineer's GitHub username?",
+      },
+    ])
+    .then((data) => {
+      const engineer = new Engineer(
+        data.engineerName,
+        data.engineerId,
+        data.engineerEmail,
+        data.gitHub
+      );
+
+      allEmployees.push(engineer);
+
+      directory();
+    });
 }
-
-function addEngineer() {}
-
 function addIntern() {}
 
 function buildHTML() {}
@@ -54,3 +92,4 @@ function buildHTML() {}
 // What do you want to do next? - add Engineer, add Intern, done adding employees
 
 addManager();
+addEngineer();
